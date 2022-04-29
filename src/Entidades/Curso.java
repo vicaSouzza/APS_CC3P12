4157b3c7ff0e5588549b51acd42f95fc2daad21d
Mantovaniz <67487622+Mantovaniz@users.noreply.github.com>
2022-04-29 20:49:56 -0300

    Update Curso.java
    Esta é a parte da Gih que a Sofia fez por que ela estava inspirada porém novamente eu subi no git da Bia, obrigado novamente :DDD

--- src/Entidades/Curso.java
+++ src/Entidades/Curso.java
@@ -1,6 +1,42 @@
-package Entidades;
+package entidade;
+
+public class Curso{
+	
+	private String nivel;
+	private String nome;
+    private Integer ano;
+	
+	public Curso (String nivel,Integer ano, String nome) {
+		this.nivel = nivel;
+		this.nome = nome;
+        this.ano= ano;
+	}
+	
+	public String getNivel() {
+		return nivel;
+	}
+
+    public String getNome() {
+		return nome;
+
+    }
 
 
-public class Curso {
+    public String getAno() {
+		return ano;
 
-}
+    }
+
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+
+    }    
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+    public void setAno(Integer ano) { 
+		this.ano = ano;
+	}
